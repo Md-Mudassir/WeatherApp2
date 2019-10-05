7aabef42934dbf259d65163b227179320cdc3896
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-05 15:24:18 +0530

    added weather api

--- js/function.js
+++ js/function.js
@@ -1,61 +1,91 @@
-// to retrieve the Celcius & Farhenite elements
-let CF = `<sup id="C" onclick="convertTempC()"> &degC |</sup><sup id="F" onclick="convertTempF()">&degF</sup>`;
+// API key from openweathermap.org
+const appKey = "82005d27a116c2880c8f0fcb866998a0";
 
-//function to retrieve the elements
-weatherApp = () => {
-  let cities = [
-    { city: "Bangalore, Karnataka", Celcius: 25, emoji: "☀️", Data: "Sunny" },
-    { city: "Chennai, Tamil Nadu", Celcius: 15, emoji: "🌧️", Data: "Rainy" },
-    { city: "Adoni, Andhra Pradesh", Celcius: 30, emoji: "☁️", Data: "Cloudy" },
-    { city: "Bellary, Karnataka", Celcius: 40, emoji: " 🌫️", Data: "Fog" }
-  ];
-  let input = document.getElementById("pick").value;
-  let filtred = cities
-    .filter(n => n.city.includes(input))
-    .map(
-      x => (
-        (city = x.city), (temp = x.Celcius), (data = x.Data), (Emoji = x.emoji)
-      )
-    );
-  document.getElementById("city").innerHTML = city;
-  document.getElementById("data").innerHTML = data;
-  document.getElementById("temp").innerHTML = `${Emoji}  ${temp} ${CF}`;
+// declaring variables
+let tempr;
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city");
+let icon = document.getElementById("data");
+let temperature = document.getElementById("temp");
+
+// to display date
+let weekdays = new Array(7);
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+let current_date = new Date();
+
+weekday_value = current_date.getDay();
+
+document.getElementById("date").innerHTML = weekdays[weekday_value];
+
+// getting an input from user
+searchInput.addEventListener("keypress", enterPressed);
+
+function enterPressed(event) {
+  if (event.key === "Enter") {
+    findWeatherDetails();
+  }
+}
+
+// API function
+findWeatherDetails = () => {
+  if (searchInput.value === "") {
+  } else {
+    let searchLink =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      searchInput.value +
+      "&appid=" +
+      appKey;
+    httpRequestAsync(searchLink, theResponse);
+  }
 };
 
-//to convert C to F and viceversa
+// parsing and assigning the objects to elements
+theResponse = response => {
+  let jsonObject = JSON.parse(response);
+  cityName.innerHTML = jsonObject.name;
+  tempr = parseInt(jsonObject.main.temp - 273);
+  icon.src =
+    "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+  temperature.innerHTML = `${tempr} ${CF}`;
+
+  if (temperature.innerHTML >= "20" && temperature.innerHTML < "30") {
+    document.getElementById("box2").style.background = "rgb(247, 226, 109)";
+  } else if (temperature.innerHTML >= "30" && temperature.innerHTML < "40") {
+    document.getElementById("box2").style.background = "rgb(255, 112, 112)";
+  } else {
+    document.getElementById("box2").style.background = "rgb(189, 183, 183)";
+  }
+};
+
+// retriving the data object
+httpRequestAsync = (url, callback) => {
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200)
+      callback(httpRequest.responseText);
+  };
+  httpRequest.open("GET", url, true); // true for asynchronous
+  httpRequest.send();
+};
+
+//Reference for Celcius & Ferhanite Letters
+let CF = `<sup id="C" onclick="convertTempC()"> &degC |</sup><sup id="F" onclick="convertTempF()">&degF</sup>`;
+
+//To convert C to F and viceversa
 convertTempF = () => {
-  let C = temp;
+  let C = tempr;
   let Fer = Math.round((C * 9) / 5 + 32);
-  document.getElementById("temp").innerHTML = `${Emoji}  ${Fer}  ${CF}`;
+  document.getElementById("temp").innerHTML = ` ${Fer}  ${CF}`;
 };
 
 convertTempC = () => {
-  let C = temp;
-  document.getElementById("temp").innerHTML = `${Emoji}  ${C}  ${CF}`;
-};
-
-// day and time functions
-addZero = i => {
-  if (i < 10) {
-    i = 0 + i;
-  }
-  return i;
-};
-
-Time = () => {
-  let weekdays = new Array(7);
-  weekdays[0] = "Sunday";
-  weekdays[1] = "Monday";
-  weekdays[2] = "Tuesday";
-  weekdays[3] = "Wednesday";
-  weekdays[4] = "Thursday";
-  weekdays[5] = "Friday";
-  weekdays[6] = "Saturday";
-
-  let d = new Date();
-  weekday_value = d.getDay();
-  let x = document.getElementById("date");
-  let h = addZero(d.getHours());
-  let m = addZero(d.getMinutes());
-  x.innerHTML = weekdays[weekday_value] + ", " + h + ":" + m;
+  let C = tempr;
+  document.getElementById("temp").innerHTML = ` ${C}  ${CF}`;
 };
